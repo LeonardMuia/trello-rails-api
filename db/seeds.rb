e91166a28d08519e19887640878d9019f7d5085d
LeonardMuia <leonardlouis25@outlook.com>
2022-09-28 01:41:51 +0300

    added materialui to frontend

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# require 'rest-client'
+
+# def api_key
+#     ENV['API_KEY']
+# end
+
+# def token 
+#     ENV['TOKEN']
+# end
+
+# def idList
+#     ENV['ID_LIST']
+# end
+
+# def idBoard
+#     ENV['ID_BOARD']
+# end
+
+# def get_lists
+    
+#     lists = RestClient.get("https://api.trello.com/1/boards/#{idBoard}/lists?key=#{api_key}&token=#{token}")
+
+#     lists_array = JSON.parse(lists)
+
+#     lists_array.each do |s|
+#         List.create(name: s["name"])
+#     end
+# end
+
+# get_lists()
