69e63d1a3dc56e42905bb64d2640e97b2bc96b2b
LeonardMuia <leonardlouis25@outlook.com>
2022-09-28 03:09:24 +0300

    added custom libraries to the app

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import 'sweetalert2/src/sweetalert2.scss'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
