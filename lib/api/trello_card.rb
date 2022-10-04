fbd2ac8db704ca5faeae16938db2cc586abfc530
LeonardMuia <leonardlouis25@outlook.com>
2022-10-04 20:11:18 +0300

    added module namespace

--- lib/api/trello_card.rb
+++ lib/api/trello_card.rb
@@ -1,9 +1,8 @@
-class TrelloCard
-    def initialize(info)
-    end
-
-    def create_list
-        cards = RestClient.get("#{ENV['BASE_URL']}/boards/#{ENV['ID_BOARD']}/cards?key=#{ENV['API_KEY']}&token=#{ENV['TOKEN']}")
-        cards_data = JSON.parse(cards)
+module Api
+    class TrelloCard
+        def create_list
+            cards = RestClient.get("#{ENV['BASE_URL']}/boards/#{ENV['ID_BOARD']}/cards?key=#{ENV['API_KEY']}&token=#{ENV['TOKEN']}")
+            cards_data = JSON.parse(cards)
+        end
     end
 end
