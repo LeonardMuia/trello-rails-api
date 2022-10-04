cbbf1d12dc4986bec08caff24959c29c55f65b8a
LeonardMuia <leonardlouis25@outlook.com>
2022-10-04 20:09:01 +0300

    removed unique column requirement

--- app/models/list.rb
+++ app/models/list.rb
@@ -3,8 +3,6 @@ require './lib/api/trello_list'
 class List < ApplicationRecord
     has_many :cards, dependent: :destroy
 
-    validates_uniqueness_of :listId
-
     def self.get_list
         list = Api::TrelloList.new
         list.create_list
