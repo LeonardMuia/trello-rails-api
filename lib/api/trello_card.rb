7da46d7953eab8791921f2eb66adb21b9c7e0582
LeonardMuia <leonardlouis25@outlook.com>
2022-10-31 18:06:22 +0300

    implemented response by code on RestClient

--- lib/api/trello_card.rb
+++ lib/api/trello_card.rb
@@ -11,8 +11,18 @@ module Api
             url = "#{ENV['BASE_URL']}/cards?key=#{ENV['API_KEY']}&token=#{ENV['TOKEN']}"
             payload = {idList: card_params[:idList], name: card_params[:name]}
 
-            response = RestClient.post(url,payload)
-            puts response
+
+            # Process the Post Request using RestClient
+            begin
+                RestClient.post(url,payload)
+            rescue RestClient::ExceptionWithResponse => err
+                case err.http_code
+                when 301,302,307
+                    err.response.follow_redirection
+                else 
+                    raise
+                end
+            end 
 
         end
     end
